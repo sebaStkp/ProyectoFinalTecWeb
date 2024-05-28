b658d4eddd4ecf3782e99c35c5753534767dde47
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-05-28 10:10:03 -0400

    Se busca los productos de manera correcta

--- src/app/paginas/buscar/buscar.component.ts
+++ src/app/paginas/buscar/buscar.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Producto } from '../../interfaces/product';
+import { ProductoComponent } from '../../elementos/producto/producto.component';
 
 @Component({
   selector: 'app-buscar',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, ProductoComponent],
   templateUrl: './buscar.component.html',
   styleUrls: ['./buscar.component.scss']
 })
 export class BuscarComponent implements OnInit {
   query: string = '';
-  results: any[] = []; // Ajusta el tipo según la estructura de tus datos
+  results: Producto[] = []; // Ajusta el tipo según la estructura de tus datos
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -24,8 +26,8 @@ export class BuscarComponent implements OnInit {
   }
 
   search(query: string) {
-    const url = `https://fakestoreapi.com/products?limit=10`; // URL de ejemplo, ajústala según tus necesidades
-    this.http.get<any[]>(url).subscribe((data) => {
+    const url = `https://fakestoreapi.com/products`; // URL de ejemplo, ajústala según tus necesidades
+    this.http.get<Producto[]>(url).subscribe((data) => {
       this.results = data.filter(item => item.title.toLowerCase().includes(query.toLowerCase()));
     });
   }
