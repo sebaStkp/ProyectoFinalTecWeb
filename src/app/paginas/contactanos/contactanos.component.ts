819ca48e9af66e944adc9665ce1f0695fcf4d960
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-06-04 17:02:18 -0400

    Formulario de Contacto

--- src/app/paginas/contactanos/contactanos.component.ts
+++ src/app/paginas/contactanos/contactanos.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+// contactanos.component.ts
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-contactanos',
-  standalone: true,
-  imports: [],
   templateUrl: './contactanos.component.html',
-  styleUrl: './contactanos.component.scss'
+  styleUrls: ['./contactanos.component.scss']
 })
 export class ContactanosComponent {
 
+  @ViewChild('nombreInput') nombreInput!: ElementRef;
+  @ViewChild('emailInput') emailInput!: ElementRef;
+  @ViewChild('mensajeInput') mensajeInput!: ElementRef;
+
+  constructor() { }
+
+  enviarFormulario(nombre: string, email: string, mensaje: string) {
+    // Aquí puedes agregar la lógica para enviar el formulario
+    console.log('Nombre:', nombre);
+    console.log('Correo Electrónico:', email);
+    console.log('Mensaje:', mensaje);
+    // Mostrar un mensaje de éxito
+    alert('¡Mensaje enviado correctamente!');
+    // Limpiar los campos
+    this.nombreInput.nativeElement.value = '';
+    this.emailInput.nativeElement.value = '';
+    this.mensajeInput.nativeElement.value = '';
+  }
 }
