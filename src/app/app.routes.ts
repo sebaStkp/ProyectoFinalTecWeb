2ac52c20ab07be11e6d2300c4bc15ed328c19868
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-06-18 06:43:30 -0400

    LogginAdmin Creado Correctamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,8 @@ import { LogginComponent } from './paginas/loggin/loggin.component';
 import { FormularioProductoComponent } from './paginas/formulario-producto/formulario-producto.component';
 import { ProductosComponent } from './productos/productos.component';
 import { RegisterComponent } from './paginas/register/register.component';
+import { Component } from '@angular/core';
+import { AdministradorComponent } from './paginas/administrador/administrador.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
@@ -21,6 +23,7 @@ export const routes: Routes = [
     { path: 'buscar', component: BuscarComponent },
     { path: 'aniadir', component: FormularioProductoComponent },
     {path:'register',component: RegisterComponent},
+    {path:'loginAdmin',component:AdministradorComponent},
     { path: '**', component: PaginaNoEncontradaComponent }
 
 ];
